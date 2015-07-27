000e3abfafc5f7a8e15c018da15b4f64b6a532a5
MaximeMorille <maxime.morille@gmail.com>
2015-07-27 09:34:41 +0200

    Add a loop getting subscribers

---
+++ Loop/CustomerProductResponseLoop.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace ProductPromotionAlert\Loop;
+
+use ProductPromotionAlert\Model\CustomerProductQuery;
+use Thelia\Core\Template\Element\BaseLoop;
+use Thelia\Core\Template\Element\LoopResult;
+use Thelia\Core\Template\Element\LoopResultRow;
+use Thelia\Core\Template\Element\PropelSearchLoopInterface;
+use Thelia\Core\Template\Loop\Argument\Argument;
+use Thelia\Core\Template\Loop\Argument\ArgumentCollection;
+
+class CustomerProductResponseLoop extends BaseLoop implements PropelSearchLoopInterface
+{
+
+    /**
+     * @param LoopResult $loopResult
+     *
+     * @return LoopResult
+     */
+    public function parseResults(LoopResult $loopResult)
+    {
+        foreach($loopResult->getResultDataCollection() as $productSubscriber) {
+            $loopResultRow = new LoopResultRow($productSubscriber);
+
+            $loopResultRow
+                ->set("PRODUCT_ID", $productSubscriber->getProductId())
+                ->set("CUSTOMER_ID", $productSubscriber->getCustomerId())
+            ;
+
+            $loopResult->addRow($loopResultRow);
+        }
+
+        return $loopResult;
+    }
+
+    /**
+     * Definition of loop arguments
+     *
+     * example :
+     *
+     * public function getArgDefinitions()
+     * {
+     *  return new ArgumentCollection(
+     *
+     *       Argument::createIntListTypeArgument('id'),
+     *           new Argument(
+     *           'ref',
+     *           new TypeCollection(
+     *
+     *               new Type\AlphaNumStringListType()
+     *           )
+     *       ),
+     *       Argument::createIntListTypeArgument('category'),
+     *       Argument::createBooleanTypeArgument('new'),
+     *       ...
+     *   );
+     * }
+     *
+     * @return \Thelia\Core\Template\Loop\Argument\ArgumentCollection
+     */
+    protected function getArgDefinitions()
+    {
+        return new ArgumentCollection(
+            Argument::createIntListTypeArgument('product_id')
+        );
+    }
+
+    /**
+     * this method returns a Propel ModelCriteria
+     *
+     * @return \Propel\Runtime\ActiveQuery\ModelCriteria
+     */
+    public function buildModelCriteria()
+    {
+        $search = CustomerProductQuery::create()
+            ->useProductQuery()
+            ->useCustomerQuery()
+            ->endUse();
+
+        if (null !== $id = $this->getProductId()) {
+            $search->filterByProductId($id, Criteria::IN);
+        }
+
+        return $search;
+    }
+}
