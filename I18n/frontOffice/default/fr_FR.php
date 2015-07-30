faeed0392c7c67fb0825f865b5595eb969f3aaf7
MaximeMorille <maxime.morille@gmail.com>
2015-07-30 13:09:23 +0200

    Add french translations

---
+++ I18n/frontOffice/default/fr_FR.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+    'You will get a notification when this product will be promoted.' => 'Vous recevrez une notification quand ce produit sera mis en promotion.',
+    'Do you want a notification when this product will be on sale ?' => 'Voulez-vous recevoir une notification quand ce produit sera soldé ?',
+    'Subscribe' => 'S\'inscrire'
+);
